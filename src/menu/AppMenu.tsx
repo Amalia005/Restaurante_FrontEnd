b26910dc643f64deb906290f37e67b74e7d2dbde
Amalia005 <amalia.toledo@estudiantes.uv.cl>
2025-12-03 03:24:06 -0300

    mejor visualización en pantallas grandes

--- src/menu/AppMenu.tsx
+++ src/menu/AppMenu.tsx
@@ -107,7 +107,7 @@ export default function AppMenu() {
    return (
     <div className="min-h-screen bg-orange-50 flex justify-center">
       {/* Contenedor tipo “celular” centrado en pantallas grandes */}
-      <div className="w-full max-w-md md:max-w-2xl lg:max-w-4xl
+      <div className="w-full text-xs md:text-sm max-w-lg md:max-w-4xl lg:max-w-7xl
         bg-orange-50">
         <MenuHeader isVisible={headerVisible} />
 
